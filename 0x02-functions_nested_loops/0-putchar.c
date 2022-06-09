6418e7e3e7e6a696aff10adf24eb63a48da60bf9
Prince-Tee <adebiyi1605@gmai.com>
2022-06-09 10:45:52 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
 #include "main.h"
 /**
-* main - check the code
-*
-* Return: Always 0.
+* main -  prints _putchar, followed by a new line
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-print_alphabet();
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
 return (0);
 }
