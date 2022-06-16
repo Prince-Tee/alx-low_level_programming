95aac642e60af9ed36937b533f0f33f4601e8c8f
Prince-Tee <adebiyi1605@gmai.com>
2022-06-15 23:06:35 -0700

    task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,16 @@ void print_number(int n)
 {
 if (n == 0)
 {
-putchar('0');
+_putchar('0');
 return;
 }
 else if (n > 0)
 n *= -1;
 else
-putchar('-');
+_putchar('-');
 if ((n / 10) != 0)
 {
 print_number((n / 10) * -1);
 }
-putchar((n % 10) * -1 + '0');
+_putchar((n % 10) * -1 + '0');
 }
