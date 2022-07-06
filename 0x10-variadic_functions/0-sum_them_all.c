8ff1c4516a3024279c3a02814fc292f8d7df87fd
Prince-Tee <adebiyi1605@gmai.com>
2022-07-06 16:09:39 -0700

    function that prints strings, followed by a new line

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
-* sum_them_all - sums all its parameters
-* @n: first parameter
+*sum_them_all - sums all its parameters
+*@n: first parameter
 *
-* Return: 0 if n is null
-* or sum of parameters in other case
+*Return: 0 if n is null
+*or sum of parameters in other case
 */
 int sum_them_all(const unsigned int n, ...)
 {
